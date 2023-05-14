4b4bb8ae25eee3fe3765b97c4d2dcdf7bc885e8c
bibrarin <parzival.rpo@gmail.com>
2023-05-14 18:10:25 +0200

    subida Reto 02

--- projets/own/retos-programacion-2023/Reto #02/bibrarin.py
+++ projets/own/retos-programacion-2023/Reto #02/bibrarin.py
@@ -16,14 +16,41 @@
 '''
 
 
-def puntoPara(jugador):
-    if (jugador=='P1'):
-        resultado = sumaJugador('P1', resultado)
-    elif (jugador=='P2'):
-        resultado = sumaJugador('P2', resultado)
+def RolangGarros(puntos):
+    lista_puntos = {0:'0', 1:'15', 2:'30', 3:'40'}
+    contador_P1 = 0
+    contador_P2 = 0
+    winner = ''
 
-    print(resultado)
+    for punto in puntos:
+        if (punto == 'P1'):
+            contador_P1 += 1
+        elif (punto == 'P2'):
+            contador_P2 += 1
+        else:
+            print('Error en la inserción de datos')
 
+        if (contador_P1 <= 3 and contador_P2 < 3 ) or (contador_P1 < 3 and contador_P2 <= 3 ):
+            print(f"{lista_puntos[contador_P1]} - {lista_puntos[contador_P2]}")
+        elif (contador_P1 == contador_P2):
+            print('Deuce')
+        elif (contador_P1 - contador_P2 == 1):
+            print('Ventaja P1')
+        elif (contador_P1 - contador_P2 == -1):
+            print('Ventaja P2')
+        else:
+            if (contador_P1 > contador_P2):
+                winner = 'Ganador es el jugador 1'
+            else:
+                winner = 'Ganador es el jugador 2'
+    if winner == ''
+        return 'Quedan puntos por disputar'
+    else:
+        return winner
 
-def sumaJugador(jugador, resultado)
-    
+def main():
+    puntos = ('P1', 'P1', 'P2', 'P2', 'P1', 'P2', 'P1', 'P2', 'P1', 'P1')
+    print(RolangGarros(puntos))
+
+if __name__ == "__main__":
+    main() 
